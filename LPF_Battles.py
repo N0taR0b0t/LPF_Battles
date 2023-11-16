270b10af7730c6ee13893809e0c1c2ef957f0d62
Matias B <57303583+N0taR0b0t@users.noreply.github.com>
2023-11-16 14:16:57 -0500

    Fix data source logic

--- LPF_Battles.py
+++ LPF_Battles.py
@@ -1,72 +1,83 @@
+import csv
 import json
-from math import radians, cos, sin, asin, sqrt
 
-def haversine(lon1, lat1, lon2, lat2):
-    """Calculate the great circle distance in miles between two points on the earth"""
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-    c = 2 * asin(sqrt(a))
-    r = 3956  # Radius of Earth in miles
-    return c * r
+def process_csv_to_json(csv_file_path):
+    features = []
 
-def combine_features(features):
-    combined_features = {}
+    with open(csv_file_path, encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)  # Skip the header row
 
-    for feature in features:
-        title = feature['properties']['title']
-        coordinates = feature['geometry']['coordinates'] if 'geometry' in feature else None
+        for row in csv_reader:
+            # Dynamically unpack the row based on the number of columns
+            (
+                id, title, _, year, _, _, country_code, _,
+                _, identifier, _, longitude, latitude, *rest
+            ) = row
 
-        if title not in combined_features:
-            combined_features[title] = feature
-        else:
-            existing_feature = combined_features[title]
-            existing_coordinates = existing_feature['geometry']['coordinates'] if 'geometry' in existing_feature else None
+            # Extract description from the rest of the row
+            description = rest[-3] if len(rest) >= 3 else ''
 
-            if coordinates and existing_coordinates:
-                distance = haversine(coordinates[0], coordinates[1], existing_coordinates[0], existing_coordinates[1])
-                if distance <= 100:
-                    # Combine features
-                    for key in feature:
-                        if key in existing_feature:
-                            if isinstance(existing_feature[key], list):
-                                # Special handling for list of dictionaries
-                                if existing_feature[key] and isinstance(existing_feature[key][0], dict):
-                                    existing_feature[key].extend(x for x in feature[key] if x not in existing_feature[key])
-                                else:
-                                    existing_feature[key] = list(set(existing_feature[key] + feature[key]))  # Merge lists without duplicates
-                            elif isinstance(existing_feature[key], dict):
-                                for sub_key in feature[key]:
-                                    if sub_key in existing_feature[key]:
-                                        if isinstance(existing_feature[key][sub_key], list):
-                                            # Special handling for list of dictionaries
-                                            if existing_feature[key][sub_key] and isinstance(existing_feature[key][sub_key][0], dict):
-                                                existing_feature[key][sub_key].extend(x for x in feature[key][sub_key] if x not in existing_feature[key][sub_key])
-                                            else:
-                                                existing_feature[key][sub_key] = list(set(existing_feature[key][sub_key] + feature[key][sub_key]))
-                                        else:
-                                            existing_feature[key][sub_key] = feature[key][sub_key]
-                                    else:
-                                        existing_feature[key][sub_key] = feature[key][sub_key]
-                        else:
-                            existing_feature[key] = feature[key]
+            # Convert longitude and latitude to float if they are not empty
+            coordinates = [float(longitude), float(latitude)] if longitude and latitude else None
 
-    return list(combined_features.values())
+            # Create the feature object
+            feature = {
+                "@id": [id],
+                "type": "Feature",
+                "properties": {
+                    "title": title,
+                    "ccodes": [country_code] if country_code else []
+                },
+                "when": {
+                    "timespans": [
+                        {
+                            "start": {
+                                "in": year
+                            }
+                        }
+                    ]
+                },
+                "types": [
+                    {
+                        "identifier": identifier,
+                        "label": "battlefield"
+                    }
+                ],
+                "names": [
+                    {
+                        "toponym": title
+                    }
+                ],
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": coordinates
+                },
+                "descriptions": [
+                    {
+                        "value": description
+                    }
+                ]
+            }
 
+            features.append(feature)
 
-# Read LPF JSON file
-with open('LPF_Battles.json', 'r', encoding='utf-8') as file:
-    lpf_data = json.load(file)
+    # Creating the final JSON structure
+    lpf_data = {
+        "type": "FeatureCollection",
+        "@context": "https://linkedpasts.org/assets/linkedplaces-context-v1.jsonld",
+        "features": features
+    }
 
-# Combine features
-combined_features = combine_features(lpf_data['features'])
+    return lpf_data
 
-# Update LPF data
-lpf_data['features'] = combined_features
+# Process the CSV file and convert it to JSON format
+csv_file_path = 'Original.csv'
+lpf_json_data = process_csv_to_json(csv_file_path)
 
-# Save combined data to new JSON file
-with open('LPF_Battles.json', 'w', encoding='utf-8') as json_file:
-    json.dump(lpf_data, json_file, indent=4)
+# Save the processed data to a new JSON file
+output_json_file_path = 'Processed_LPF.json'
+with open(output_json_file_path, 'w', encoding='utf-8') as json_file:
+    json.dump(lpf_json_data, json_file, indent=4)
 
-print(f"Combined {len(lpf_data['features'])} features")
+output_json_file_path
