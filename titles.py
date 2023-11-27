7aeeb2178e4f3975cbd433db26804109b4929ee0
Matias B <57303583+N0taR0b0t@users.noreply.github.com>
2023-11-27 11:06:29 -0500

    Update titles.py

--- titles.py
+++ titles.py
@@ -5,8 +5,13 @@ def read_json_and_find_duplicates(json_file_path):
     with open(json_file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    # Extracting titles
-    titles = [feature['properties']['title'] for feature in data['features']]
+    titles = []
+    for feature in data['features']:
+        if 'title' in feature['properties']:
+            titles.append(feature['properties']['title'])
+        else:
+            # Warning for missing 'title'
+            print(f"Warning: Missing 'title' in feature with ID {feature.get('@id', 'Unknown')}")
 
     # Counting occurrences of each title
     title_counts = Counter(titles)
@@ -17,10 +22,9 @@ def read_json_and_find_duplicates(json_file_path):
     return non_unique_titles
 
 # Path to the JSON file
-json_file_path = 'LPF_Battles.json'
+json_file_path = 'Processed_LPF.json'
 
 # Find and display non-unique titles
 duplicates = read_json_and_find_duplicates(json_file_path)
 for title, count in duplicates.items():
     print(f"'{title}' appears {count} times")
-
