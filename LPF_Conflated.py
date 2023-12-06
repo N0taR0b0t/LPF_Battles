15c3411512bf87999404ab1da003d7128f7961d2
Matias B <57303583+N0taR0b0t@users.noreply.github.com>
2023-12-06 16:44:26 -0500

    Add ToDos

--- LPF_Conflated.py
+++ LPF_Conflated.py
@@ -2,23 +2,26 @@ import csv
 import json
 import logging
 
-# Set up logging
+#TODO Kandahar is gone, put it back
+#TODO: Fix IDs such that they are integers
+#TODO: Possibly convert coordinates to miles, as longitude changes with the curves of the Earth
+#Use the proper GIS coordinate system
+#Make sure the years go from lowest to greatest and titles matches order
+#Add battles year to value description
+
 logging.basicConfig(filename='lpf_processing.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def are_coordinates_close(coord1, coord2, threshold=0.01):
-    """Check if two coordinates are within a certain threshold."""
+def are_coordinates_close(coord1, coord2, threshold=0.1):
     if coord1 is None or coord2 is None:
         return False
     return abs(coord1[0] - coord2[0]) <= threshold and abs(coord1[1] - coord2[1]) <= threshold
 
 def process_csv_conflate_duplicates(csv_file_path):
-    # List to store all events
     events = []
 
-    # Read CSV file
     with open(csv_file_path, encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file)
-        next(csv_reader)  # Skip the header row
+        next(csv_reader)
 
         for row in csv_reader:
             (
@@ -40,20 +43,32 @@ def process_csv_conflate_duplicates(csv_file_path):
             }
             events.append(event)
 
-    # Dictionary to store conflated events keyed by title
     conflated_events = {}
 
     for event in events:
         title = event["title"]
+        coordinates = event["coordinates"]
+
         if title in conflated_events:
-            # Conflate data for duplicates
             existing_event = conflated_events[title]
-            existing_event['timespans'].add(event['start'])
-            existing_event['descriptions'].add(event['description'])
-            existing_event['battle_count'] += 1
-            logging.debug(f"Conflated duplicate: {title}")
+            existing_coordinates = existing_event['coordinates']
+
+            if are_coordinates_close(coordinates, existing_coordinates):
+                existing_event['timespans'].add(event['start'])
+                existing_event['descriptions'].add(event['description'])
+                existing_event['battle_count'] += 1
+            else:
+                modified_title = f"{title}_{coordinates}"
+                conflated_events[modified_title] = {
+                    'timespans': {event['start']},
+                    'descriptions': {event['description']},
+                    'battle_count': 1,
+                    'country_code': event['country_code'],
+                    'identifier': event['identifier'],
+                    'coordinates': event['coordinates']
+                }
+
         else:
-            # Add new event
             conflated_events[title] = {
                 'timespans': {event['start']},
                 'descriptions': {event['description']},
@@ -63,11 +78,10 @@ def process_csv_conflate_duplicates(csv_file_path):
                 'coordinates': event['coordinates']
             }
 
-    # Creating the final JSON structure from conflated events
     features = []
     for title, data in conflated_events.items():
         feature = {
-            "@id": title,  # Using title as unique identifier might be a simplification
+            "@id": title,
             "type": "Feature",
             "properties": {
                 "title": title,
@@ -99,11 +113,9 @@ def process_csv_conflate_duplicates(csv_file_path):
 
     return lpf_data
 
-# Process the CSV file and convert it to JSON format
 csv_file_path = 'Original.csv'
 lpf_json_data = process_csv_conflate_duplicates(csv_file_path)
 
-# Save the processed data to a new JSON file
 output_json_file_path = 'Processed_LPF_Conflated.json'
 with open(output_json_file_path, 'w', encoding='utf-8') as json_file:
     json.dump(lpf_json_data, json_file, indent=4)
